508696f0c896488f5d5b71f4245d51125c61603c
Xiangyi Meng <xymeng16@gmail.com>
2024-03-29 04:11:36 +0800

    nvim/plugins: make nvim-treesitter work

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+if true then
+  return {}
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    c = {
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
+    configs.setup(c)
+    if vim.fn.has("win32") then
+      c.compilers = { "clang" }
+    end
+  end,
+}
