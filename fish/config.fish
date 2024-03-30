8c432012cd48cb51f4021f2a5bf02b0944930b8a
Xiangyi Meng <xymeng16@gmail.com>
2024-03-30 21:50:43 +0800

    fish/conf: add platform-dependent scripts

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here 
+    # detect OS type
+    set OS (uname)
+    switch $OS
+        case Linux
+        case Darwin
+            # setup homebrew
+            # check if homebrew is installed
+            if test -e /opt/homebrew/bin/brew
+                fish_add_path /opt/homebrew/bin
+            end
+    end
+    # setup miniconda if installed
+    if test -e ~/miniconda3
+        fish_add_path ~/miniconda3/bin
+    end
     oh-my-posh init fish --config "~/.config/fish/omp/1_shell.omp.json" | source
-    eval "$(/home/xiangyi/miniconda3/bin/conda shell.fish hook)"
 end
