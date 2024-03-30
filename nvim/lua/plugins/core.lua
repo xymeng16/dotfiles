47c2a14cd1f459cf1b704fdb53bf7248f4162dab
Xiangyi Meng <xymeng16@gmail.com>
2024-03-31 02:55:43 +0800

    nvim/lazyvim: disable news

---
+++ nvim/lua/plugins/core.lua
@@ -0,0 +1,16 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+    news = {
+      -- When enabled, NEWS.md will be shown when changed.
+      -- This only contains big new features and breaking changes.
+      lazyvim = false,
+      -- Same but for Neovim's news.txt
+      neovim = false,
+    },
+  },
+}
