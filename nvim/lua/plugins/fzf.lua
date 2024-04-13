a5c3ba84b5a33e07f24a3bc2eba63878a324ba1b
Xiangyi Meng <xymeng16@gmail.com>
2024-04-13 19:16:20 +0800

    nvim/plugin: try to add key binding for on-the-fly toggling hidden files in fzf

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -4,6 +4,20 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
+    require("fzf-lua").setup({
+      debug = true,
+      action = {
+        ["ctrl-h"] = {
+          function(_, args)
+            if args.cmd:find("--hidden") then
+              args.cmd = args.cmd:gsub("--hidden", "", 1)
+            else
+              args.cmd = args.cmd .. " --hidden"
+            end
+            require("fzf-lua").files(args)
+          end,
+        },
+      },
+    })
   end,
 }
