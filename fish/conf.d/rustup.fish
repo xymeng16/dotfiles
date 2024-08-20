1a33c59bb537c89255be293b37071aefbaca2712
Xiangyi Meng <xymeng16@gmail.com>
2024-08-20 15:58:59 +0800

    fish: add rustup config

--- fish/conf.d/rustup.fish
+++ fish/conf.d/rustup.fish
@@ -3,3 +3,4 @@ if not contains "$HOME/.cargo/bin" $PATH
     # Prepending path in case a system-installed rustc needs to be overridden
     set -x PATH "$HOME/.cargo/bin" $PATH
 end
+source "$HOME/.cargo/env.fish"
