29164b62dcdbbad4ddb1c07764f59e4fcf762f63
Xiangyi Meng <xymeng16@gmail.com>
2025-04-13 15:56:33 +0800

    nvim: setup clangd lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -8,6 +8,42 @@ local config = {
         enabled = false,
       },
       servers = {
+        clangd = {
+          keys = {
+            { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(fname)
+            return vim.fn["getcwd"]()
+            -- return require("lspconfig.util").root_pattern(
+            --   "Makefile",
+            --   "configure.ac",
+            --   "configure.in",
+            --   "config.h.in",
+            --   "meson.build",
+            --   "meson_options.txt",
+            --   "build.ninja"
+            -- )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+            --   fname
+            -- ) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
         texlab = {
           keys = {
             { "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
@@ -66,6 +102,11 @@ local config = {
         },
       },
       setup = {
+        clangd = function(_, opts)
+          local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+          return false
+        end,
         gopls = function(_, opts)
           -- workaround for gopls not supporting semanticTokensProvider
           -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
